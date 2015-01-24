9cdf1a5dae8b4c8db30f1b8bb09c7e903b3e2f3a
pcofilada <pcofilada@gmail.com>
2015-01-24 17:59:04 +0800

    Add other methods for hospital

--- app/api/v1/healthcare/hospitals.rb
+++ app/api/v1/healthcare/hospitals.rb
@@ -12,6 +12,28 @@ module V1
 				get ':hospital' do
 					Hospital.find(params[:hospital])
 				end
+
+				desc 'Create new hospital'
+				post 'new' do
+					Hospital.create!({
+						name: params[:name],
+						rhu_code: params[:rhu_code]
+						})
+				end
+
+				desc 'Update hospital'
+				put ':hospital' do
+					Hospital.find(params[:hospital]).update(
+						name: params[:name],
+						rhu_code: params[:rhu_code]
+						)
+				end
+
+				desc 'Delete hospital'
+				delete ':hospital' do
+					Hospital.find(params[:hospital]).destroy!
+				end
+
 			end
 		end
 	end
