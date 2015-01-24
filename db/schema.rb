9b4aea2ecd4dfe81f75818e22a517f570855167d
pcofilada <pcofilada@gmail.com>
2015-01-24 14:56:54 +0800

    Add password to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124061310) do
+ActiveRecord::Schema.define(version: 20150124065634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150124061310) do
     t.date     "birth_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password", limit: 1
   end
 
 end
