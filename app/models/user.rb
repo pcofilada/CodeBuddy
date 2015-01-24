42c1435645eb370f77ef12a9855e5b9f34aa1002
pcofilada <pcofilada@gmail.com>
2015-01-24 15:43:52 +0800

    Add role model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
 	has_one :baranggay
+	has_one :role
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124073146) do
+ActiveRecord::Schema.define(version: 20150124074249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150124073146) do
   add_index "baranggays", ["municipality_id"], name: "index_baranggays_on_municipality_id", using: :btree
 
   create_table "municipalities", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at"
